60a12434a46fdd7425221a78fef6e8ac4cdea94a
saurabh sharma <saurabhshrm65@gmail.com>
2024-11-25 00:03:09 +0530

    auction.py

--- auction.py
+++ auction.py
@@ -6,14 +6,14 @@ def find_highest_bidder(bidding_record):
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
-    print(f"The winner is {winner} with a bid of {highest_bid}")
+    print(f"The winner is {winner} with a bid of ₹{highest_bid}")
 
 
 bids = {}
 continue_bidding = True
 while continue_bidding:
     name = input("What is your name?: ")
-    price = int(input("What is your bid?: "))
+    price = int(input("What is your bid?: ₹ "))
     bids[name] = price
     should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
     if should_continue == "no":
